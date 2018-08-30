ba668a4fc20cedfcc7b6b2ab200f47e697ab2137
Shane Bracher <sbracher@gmail.com>
2018-08-30 17:06:23 +1000

    Added c++ object-oriented version of number_counter (oop_number_counter.cpp)

---
+++ build/my_robot_tutorials/CMakeFiles/oop_number_counter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oop_number_counter.dir/src/oop_number_counter.cpp.o"
+  "/home/ros/catkin_ws/devel/lib/my_robot_tutorials/oop_number_counter.pdb"
+  "/home/ros/catkin_ws/devel/lib/my_robot_tutorials/oop_number_counter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oop_number_counter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
