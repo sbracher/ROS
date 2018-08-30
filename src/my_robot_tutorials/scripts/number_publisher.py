7e2fe48ba3b7b2660d08db4d16055992f7b31886
Shane Bracher <sbracher@gmail.com>
2018-08-30 11:33:58 +1000

    Updated number_publisher.py to get/set ROS parameters

--- src/my_robot_tutorials/scripts/number_publisher.py
+++ src/my_robot_tutorials/scripts/number_publisher.py
@@ -9,10 +9,15 @@ if __name__ == '__main__':
 
     pub = rospy.Publisher("/number", Int64, queue_size=10)
 
-    rate = rospy.Rate(1)
+    publish_frequency = rospy.get_param("/number_publish_frequency")
+    rate = rospy.Rate(publish_frequency)
+
+    number = rospy.get_param("/number_to_publish")
+
+    rospy.set_param("/another_param", "Hello")
 
     while not rospy.is_shutdown():
         msg = Int64()
-        msg.data = 2
+        msg.data = number
         pub.publish(msg)
         rate.sleep()
